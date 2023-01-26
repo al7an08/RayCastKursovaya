9e8387fba171e97159587d1f3a5837fa3b206646
Altan <suyanovaltan@gmail.com>
2023-01-26 17:19:39 +0300

    Добавлены кнопки в меню

--- src/Menu.cpp
+++ src/Menu.cpp
@@ -4,15 +4,15 @@
 Menu::Menu() {
 	isMenu = 1;
 	Texture_Button1.loadFromFile("Resources/Textures/Button1.png");
-	Texture_Button2.loadFromFile("Resources/Textures/Button1.png");
-	Texture_Button3.loadFromFile("Resources/Textures/Button1.png");
+	Texture_Button2.loadFromFile("Resources/Textures/Button2.png");
+	Texture_Button3.loadFromFile("Resources/Textures/Button3.png");
 }
 
 void Menu::DrawMenu(sf::RenderWindow& i_window) {
-	sf::Sprite menu1(Texture_Button1), menu2(Texture_Button1), menu3(Texture_Button1), about(Texture_Button1);
-	menu1.scale(5.f, 5.f);
+	sf::Sprite menu1(Texture_Button1), menu2(Texture_Button2), menu3(Texture_Button3), about(Texture_Button1);
+	/*menu1.scale(5.f, 5.f);
 	menu2.scale(5.f, 5.f);
-	menu3.scale(5.f, 5.f);
+	menu3.scale(5.f, 5.f);*/
 	int menuNum = 0;
 	menu1.setPosition(- menu1.getGlobalBounds().width / 2 + SCREEN_WIDTH / 2, SCREEN_HEIGHT / 5);
 	menu2.setPosition(- menu2.getGlobalBounds().width / 2 + SCREEN_WIDTH / 2, 2 * SCREEN_HEIGHT / 5);
@@ -20,13 +20,25 @@ void Menu::DrawMenu(sf::RenderWindow& i_window) {
 	menu1.setColor(sf::Color::White);
 	menu2.setColor(sf::Color::White);
 	menu3.setColor(sf::Color::White);
-	menuNum = 0;
+	
 	i_window.clear(sf::Color(129, 181, 221));
 	
 
-	if (sf::IntRect(100, 30, 300, 50).contains(sf::Mouse::getPosition(i_window))) { menu1.setColor(sf::Color::Blue); menuNum = 1; }
-	if (sf::IntRect(100, 90, 300, 50).contains(sf::Mouse::getPosition(i_window))) { menu2.setColor(sf:: Color::Blue); menuNum = 2; }
-	if (sf::IntRect(100, 150, 300, 50).contains(sf::Mouse::getPosition(i_window))) { menu3.setColor(sf::Color::Blue); menuNum = 3; }
+	if (sf::IntRect(-menu1.getGlobalBounds().width / 2 + SCREEN_WIDTH / 2, SCREEN_HEIGHT / 5, +menu1.getGlobalBounds().width, menu1.getGlobalBounds().height).contains(sf::Mouse::getPosition(i_window)))
+	{ 
+		menu1.setColor(sf::Color::Blue); 
+		menuNum = 1; 
+	}
+	if (sf::IntRect(-menu2.getGlobalBounds().width / 2 + SCREEN_WIDTH / 2, 2 * SCREEN_HEIGHT / 5, +menu2.getGlobalBounds().width, menu2.getGlobalBounds().height).contains(sf::Mouse::getPosition(i_window)))
+	{ 
+		menu2.setColor(sf:: Color::Blue); 
+		menuNum = 2; 
+	}
+	if (sf::IntRect(-menu3.getGlobalBounds().width / 2 + SCREEN_WIDTH / 2, 3 * SCREEN_HEIGHT / 5, +menu3.getGlobalBounds().width, menu3.getGlobalBounds().height).contains(sf::Mouse::getPosition(i_window)))
+	{ 
+		menu3.setColor(sf::Color::Blue); 
+		menuNum = 3; 
+	}
 
 	if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
 	{
